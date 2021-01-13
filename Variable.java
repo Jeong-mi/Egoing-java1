61697734a033264a68c422d96711c4cd150b286d
Jeong-mi <wjdal6951@naver.com>
2021-01-13 23:40:51 +0900

    Data_and_Operation

---
+++ Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		//데이터타입을 명시하면 값에 들어가는 것이 무엇인지를 확신할 수 있다
+		//변수의 데이터 타입을 바로바로 판단할 수 있기 때문입니다.
+		//컵에 든 음료수와 음료수병에 든 음료수, 무엇이 확실할까?
+		int a = 1; //Number -> integer ... -2, -1, 0, 1, 2 ...
+		System.out.println(a);
+		
+		double b = 1.1; //real number -> double ... -2.0, -1.0, 0, 1.0, 2.0 ...
+		System.out.println(b);
+		
+		String c = "Hello world";
+		System.out.println(c);
+
+	}
+
+}
